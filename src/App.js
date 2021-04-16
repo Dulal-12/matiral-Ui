bd5caf94675c1ced4c55b7a56b7d98fd728d16bb
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-04-16 20:43:54 +0600

    start learning matrial -ui

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Button from '@material-ui/core/Button';
+import SaveIcon from '@material-ui/icons/Save';
+import Checkbox from '@material-ui/core/Checkbox';
+import { useState } from 'react';
+import TextField from '@material-ui/core/TextField';
+
 
 function App() {
+  const [checked , setChecked] = useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    < div className="App">
+   
+     <ButtonGroup>
+     <Button
+       variant="contained" 
+       color="primary"
+       onClick={()=> alert("Hello Guys , alert")}
+       size="large"
+       style={{fontSize:4}}
+       startIcon={<SaveIcon/>}>Hello , Fiverr</Button>
+
+
+   <Button
+       variant="contained" 
+       color="secondary"
+       onClick={()=> alert("Hello Guys , alert")}
+       size="large"
+       style={{fontSize:4}}
+       startIcon={<SaveIcon/>}>Hello ,City University</Button>
+     </ButtonGroup><br/>
+     <Checkbox
+     checked={checked}
+     color="primary"
+     onClick={()=> setChecked(!checked)}
+     />
+
+
+     <br/>
+     <TextField
+     variant="outlined"
+     color="secondary"
+     type="email"
+     label="Email"
+      placeholder="test@gmail.com"/>
     </div>
   );
 }
